277d6fbcc0208922e681dc1da9faca04cac886a1
Capataz Prueba <elCapataz.P@gmail.com>
2021-05-02 19:02:35 -0500

    Is this it?

--- classes/arcanum.js
+++ classes/arcanum.js
@@ -29,7 +29,7 @@ function includeHTML() {
 function resizeIframe(e){
     console.log(e);
     var iframe = document.getElementById("d1");
-    console.log(iframe.contentWindow.body.scrollHeight);
+    console.log(iframe.contentDocument.body.scrollHeight);
     iframe.height = iframe.contentWindow.document.body.scrollHeight;
 }
 
