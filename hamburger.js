368a2ca4f244fbe5bb097f98fc4bddba5759f2e2
Capataz Prueba <elCapataz.P@gmail.com>
2021-06-15 02:15:55 -0500

    Fixed hamburger

--- hamburger.js
+++ hamburger.js
@@ -1,12 +1,24 @@
 let MenuActive = false;
 
 const menuBtn = document.querySelector('.mobile_menu_btn');
-const menuContent = document.querySelector('.mobile_nav_list');
-var menuTxt = document.querySelectorAll('.mobile_nav_a');
-const overlay = document.querySelector('.overlay');
+// const menuContent = document.querySelector('.mobile_nav_list');
+// var menuTxt = document.querySelectorAll('.mobile_nav_a');
+// const overlay = document.querySelector('.overlay');
 
-menuBtn.addEventListener('click', () => {
+menuBtn.addEventListener('click', () => {MenuActive = hamburgerDynamic(MenuActive,null);});
+
+function hamburgerDynamic(MenuActive, location){
+
+  const menuBtn = document.querySelector('.mobile_menu_btn');
+  const menuContent = document.querySelector('.mobile_nav_list');
+  var menuTxt = document.querySelectorAll('.mobile_nav_a');
+  const overlay = document.querySelector('.overlay');
+
   MenuActive = !MenuActive;
+
+  if (location) {
+    document.getElementById(location.slice(1)).scrollIntoView({behavior: 'smooth'});
+  }
 
   if(MenuActive){
     menuBtn.classList.add('open');
@@ -26,4 +38,32 @@ menuBtn.addEventListener('click', () => {
       menuTxt[i].style.display = 'none';
     }
   }
-});
+
+  return MenuActive;
+}
+
+const mobileNav = document.querySelectorAll(".m_nav_list");
+mobileNav.forEach(element => element.addEventListener('touchstart', () => {MenuActive = hamburgerDynamic(true, element.children[0].hash);}));
+mobileNav.forEach(element => element.addEventListener('click', () => {MenuActive = hamburgerDynamic(true, element.children[0].hash);}));
+
+
+// MenuActive = !MenuActive;
+//
+// if(MenuActive){
+//   menuBtn.classList.add('open');
+//   menuContent.classList.add('menu_open');
+//   overlay.style.display = 'block';
+//
+//   for(var i = 0; i < menuTxt.length; i++){
+//     menuTxt[i].style.display = 'block';
+//   }
+// }
+// else{
+//   menuBtn.classList.remove('open');
+//   menuContent.classList.remove('menu_open');
+//   overlay.style.display = 'none';
+//
+//   for(var i = 0; i < menuTxt.length; i++){
+//     menuTxt[i].style.display = 'none';
+//   }
+// }
